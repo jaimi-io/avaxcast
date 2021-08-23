695ce9cf7e377dfd67410f2e0c8f2e73b94caa5d
Jaimi Patel <jaimip34@gmail.com>
2021-08-23 20:23:31 +0100

    feat(PredictionMarket.sol): Add prediction market contract

---
+++ contracts/PredictionMarket.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.7;
+
+/** @title Prediction Market Contract
+ *  @notice Prediction markets for cryptocurrency pairs
+ *  @author Jaimi Patel
+ */
+contract PredictionMarket {
+  /**
+   * @notice Construct a new prediction market
+   */
+  constructor() {}
+}
