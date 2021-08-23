db35ade913b5f4645e30f058a99b689334a5a238
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-23 22:28:44 +0100

    chore(dummy.ts): Initial commit

---
+++ frontend/src/dummy.ts
@@ -0,0 +1,46 @@
+const IMAGE =
+  "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency/icons-390.jpg";
+
+export interface PostsT {
+  title: string;
+  info: string;
+  image: string;
+}
+
+export const posts = [
+  {
+    title: "First Crypto Coin",
+    info: "This is my first crypto coin with more content inside",
+    image: IMAGE,
+  },
+
+  {
+    title: "Second Crypto Coin",
+    info: "This is my second crypto coin with more content inside",
+    image: IMAGE,
+  },
+
+  {
+    title: "Third Crypto Coin",
+    info: "This is my third crypto coin with more content inside",
+    image: IMAGE,
+  },
+
+  {
+    title: "Fourth Crypto Coin",
+    info: "This is my fourth crypto coin with more content inside",
+    image: IMAGE,
+  },
+
+  {
+    title: "Fifth Crypto Coin",
+    info: "This is my fifth crypto coin with more content inside",
+    image: IMAGE,
+  },
+
+  {
+    title: "Sixth Crypto Coin",
+    info: "This is my sixth crypto coin with more content inside",
+    image: IMAGE,
+  },
+];
