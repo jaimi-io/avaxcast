c6e20837879f86f88ba91af9193425a47aae0770
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-23 13:01:06 +0100

    chore(Homes.tsx): Initial commit

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <h1>Total Account Value</h1>
+      <h1>Positions</h1>
+      <h1>Available Funds</h1>
+      <h1>Wallet</h1>
+    </div>
+  );
+}
