bd9d709ca624a870aba626e47c7a999dd4db2046
Jaimi Patel <jaimip34@gmail.com>
2021-08-23 11:32:33 +0100

    style(avaxcast): Format files
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # avaxcast
+
 A prediction market for cryptocurrency pair prices, built on Avalanche &amp; uses Chainlink price feeds to resolve markets.
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,17 @@
-import { 
-  Contract, 
-  ContractFactory 
-} from "ethers"
-import { ethers } from "hardhat"
+import { Contract, ContractFactory } from "ethers";
+import { ethers } from "hardhat";
 
-const main = async(): Promise<any> => {
-  const Coin: ContractFactory = await ethers.getContractFactory("ExampleERC20")
-  const coin: Contract = await Coin.deploy()
+const main = async (): Promise<any> => {
+  const Coin: ContractFactory = await ethers.getContractFactory("ExampleERC20");
+  const coin: Contract = await Coin.deploy();
 
-  await coin.deployed()
-  console.log(`Coin deployed to: ${coin.address}`)
-}
+  await coin.deployed();
+  console.log(`Coin deployed to: ${coin.address}`);
+};
 
 main()
-.then(() => process.exit(0))
-.catch(error => {
-  console.error(error)
-  process.exit(1)
-})
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
