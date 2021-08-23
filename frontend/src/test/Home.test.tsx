62c70a654856e301a0eb26ec7b26787b76e1ad6d
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-23 21:43:58 +0100

    test(Home.test.tsx): Initial commit

--- frontend/src/test/Home.test.tsx
+++ frontend/src/test/Home.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "App";
+import Home from "components/Home";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Home />);
+  const linkElement = screen.getByText(/Vote/i);
   expect(linkElement).toBeInTheDocument();
 });
