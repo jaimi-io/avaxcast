67ea5edc47ca992cc6c47d2f2b703683e17e460f
Jaimi Patel <jaimip34@gmail.com>
2021-08-24 15:55:11 +0100

    test(PredictionMarket.ts): Add test for buy shares function

--- test/PredictionMarket.ts
+++ test/PredictionMarket.ts
@@ -8,6 +8,11 @@ enum Market {
   LinkUsd,
 }
 
+enum Vote {
+  Yes = 0,
+  No,
+}
+
 describe("PredictionMarket", function () {
   before(async function () {
     this.PredictionMarket = await ethers.getContractFactory(
@@ -16,6 +21,7 @@ describe("PredictionMarket", function () {
   });
 
   beforeEach(async function () {
+    [this.owner, this.addr1] = await ethers.getSigners();
     this.predictionMarket = await this.PredictionMarket.deploy(Market.AvaxUsd);
     await this.predictionMarket.deployed();
   });
@@ -23,6 +29,49 @@ describe("PredictionMarket", function () {
   describe("Constructor", function () {
     it("Should set market correctly", async function () {
       expect(await this.predictionMarket.market()).to.equal(Market.AvaxUsd);
+    });
+  });
+
+  describe("Buy Shares", function () {
+    it("Should set total number of yes votes correctly", async function () {
+      await this.predictionMarket.buyShares(Vote.Yes, {
+        value: ethers.utils.parseEther("0.05"),
+      });
+      expect(await this.predictionMarket.numberShares(Vote.Yes)).to.equal(5);
+    });
+
+    it("Should set total number of no votes correctly", async function () {
+      await this.predictionMarket.buyShares(Vote.No, {
+        value: ethers.utils.parseEther("0.1"),
+      });
+      await this.predictionMarket.connect(this.addr1).buyShares(Vote.No, {
+        value: ethers.utils.parseEther("0.03"),
+      });
+      expect(await this.predictionMarket.numberShares(Vote.No)).to.equal(13);
+    });
+
+    it("Should set number of votes individually correctly", async function () {
+      await this.predictionMarket.buyShares(Vote.No, {
+        value: ethers.utils.parseEther("0.05"),
+      });
+      await this.predictionMarket.connect(this.addr1).buyShares(Vote.No, {
+        value: ethers.utils.parseEther("0.02"),
+      });
+      await this.predictionMarket.connect(this.addr1).buyShares(Vote.Yes, {
+        value: ethers.utils.parseEther("0.03"),
+      });
+      expect(
+        await this.predictionMarket.sharesPerPerson(this.owner.address, Vote.No)
+      ).to.equal(5);
+      expect(
+        await this.predictionMarket.sharesPerPerson(this.addr1.address, Vote.No)
+      ).to.equal(2);
+      expect(
+        await this.predictionMarket.sharesPerPerson(
+          this.addr1.address,
+          Vote.Yes
+        )
+      ).to.equal(3);
     });
   });
 });
