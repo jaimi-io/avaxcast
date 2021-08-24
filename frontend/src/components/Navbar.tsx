2c37dd58c06d81618268becfa67029ebb0d02935
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-24 15:52:51 +0100

    feat(Navbar.tsx): Add `LinkContainer`

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,6 +5,8 @@ import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
 import BookIcon from "@material-ui/icons/Book";
 import HowToVoteIcon from "@material-ui/icons/HowToVote";
 import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
+import { LinkContainer } from "react-router-bootstrap";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,27 +19,31 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function Navbar(): JSX.Element {
   const classes = useStyles();
   return (
-    <div>
+    <Router>
       <List component="nav">
         <ListItem component="div">
           <ListItemText inset>
-            <Button
-              variant="contained"
-              color="inherit"
-              className={classes.button}
-              startIcon={<HowToVoteIcon />}>
-              Vote
-            </Button>
+            <LinkContainer to="/">
+              <Button
+                variant="contained"
+                color="inherit"
+                className={classes.button}
+                startIcon={<HowToVoteIcon />}>
+                Vote
+              </Button>
+            </LinkContainer>
           </ListItemText>
 
           <ListItemText inset>
-            <Button
-              variant="contained"
-              color="inherit"
-              className={classes.button}
-              startIcon={<BookIcon />}>
-              Portfolio
-            </Button>
+            <LinkContainer to="/portfolio">
+              <Button
+                variant="contained"
+                color="inherit"
+                className={classes.button}
+                startIcon={<BookIcon />}>
+                Portfolio
+              </Button>
+            </LinkContainer>
           </ListItemText>
 
           <ListItemText inset>
@@ -61,6 +67,6 @@ export default function Navbar(): JSX.Element {
           </ListItemText>
         </ListItem>
       </List>
-    </div>
+    </Router>
   );
 }
