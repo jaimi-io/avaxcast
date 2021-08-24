636cd6c96ae2210f66f261993b4569434c12eb1f
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-24 15:53:40 +0100

    feat(Navigation.tsx): Add `/portfolio` `Route`

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Markets from "./Markets";
+import Markets from "components/Markets";
+import Portfolio from "components/Portfolio";
 
 export default function Navigation(): JSX.Element {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Markets}></Route>
+        <Route exact path="/" component={Markets} />
+        <Route exact path="/portfolio" component={Portfolio}></Route>
       </Switch>
     </Router>
   );
