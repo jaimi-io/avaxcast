082550cff4d9f289a2e0b1a221565b960b4d0026
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-24 10:33:04 +0100

    test(Home.test.tsx): Rename to Navbar.test.tsx

--- frontend/src/test/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "components/Home";
-
-test("renders learn react link", () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/Vote/i);
-  expect(linkElement).toBeInTheDocument();
-});
