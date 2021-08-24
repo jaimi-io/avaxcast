f1e0aa6cfd3339825952b976e94b8b961f2586ad
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-24 13:10:47 +0100

    refactor(Navbar.tsx): Add Icon buttons

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,37 +1,63 @@
-import { List, ListItem, ListItemText, Typography } from "@material-ui/core";
+import { List, ListItem, ListItemText } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
 import BookIcon from "@material-ui/icons/Book";
 import HowToVoteIcon from "@material-ui/icons/HowToVote";
 import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      margin: theme.spacing(1),
+    },
+  })
+);
+
 export default function Navbar(): JSX.Element {
+  const classes = useStyles();
   return (
     <div>
       <List component="nav">
         <ListItem component="div">
           <ListItemText inset>
-            <Typography color="inherit">
+            <Button
+              variant="contained"
+              color="inherit"
+              className={classes.button}
+              startIcon={<HowToVoteIcon />}>
               Vote
-              <HowToVoteIcon />
-            </Typography>
+            </Button>
           </ListItemText>
 
           <ListItemText inset>
-            <Typography color="inherit">
-              Portfolio <BookIcon />
-            </Typography>
+            <Button
+              variant="contained"
+              color="inherit"
+              className={classes.button}
+              startIcon={<BookIcon />}>
+              Portfolio
+            </Button>
           </ListItemText>
 
           <ListItemText inset>
-            <Typography color="inherit">
-              Add <LibraryAddIcon />
-            </Typography>
+            <Button
+              variant="contained"
+              color="inherit"
+              className={classes.button}
+              startIcon={<LibraryAddIcon />}>
+              Add
+            </Button>
           </ListItemText>
 
           <ListItemText inset>
-            <Typography color="inherit">
-              Wallet <AccountBalanceWalletIcon />
-            </Typography>
+            <Button
+              variant="contained"
+              color="inherit"
+              className={classes.button}
+              startIcon={<AccountBalanceWalletIcon />}>
+              Wallet
+            </Button>
           </ListItemText>
         </ListItem>
       </List>
