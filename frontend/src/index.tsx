bbd003bcccfa9afdf3cbb4dd11e103cbccebad97
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-24 16:33:18 +0100

    feat(index.tsx): Add dark theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,30 @@
+import { ThemeProvider } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+import Navbar from "components/Navbar";
+import Navigation from "components/Navigation";
+import "index.css";
 // eslint-disable-next-line no-use-before-define
 import React from "react";
-import Navbar from "components/Navbar";
 import ReactDOM from "react-dom";
-import Navigation from "components/Navigation";
+
+// if (darkMode) {
+//   document.body.classList.add("dark");
+// } else {
+//   document.body.classList.remove("dark");
+// }
+
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
-    <Navigation />
+    <ThemeProvider theme={darkTheme}>
+      <Navbar />
+      <Navigation />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
