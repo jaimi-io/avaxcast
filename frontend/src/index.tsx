0c5b81d61bcee8609165b6c6b623d6b1b957d5d0
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-24 11:27:58 +0100

    feat(index.tsx): Add `<Navigation />`

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import Navbar from "components/Navbar";
 import ReactDOM from "react-dom";
+import Navigation from "components/Navigation";
 
 ReactDOM.render(
   <React.StrictMode>
     <Navbar />
+    <Navigation />
   </React.StrictMode>,
   document.getElementById("root")
 );
