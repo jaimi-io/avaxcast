9fd26f621dbf5dbebd8b3673937b5c3434ba6d02
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-25 12:27:43 +0100

    refactor(index.tsx): Move code to `App.tsx`

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,41 +1,11 @@
-import { ThemeProvider } from "@material-ui/core";
-import { createTheme } from "@material-ui/core/styles";
-import Navigation from "components/Navigation";
-import "index.css";
+import App from "App";
 // eslint-disable-next-line no-use-before-define
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import allReducers from "reducers/";
-import { createStore } from "redux";
-
-const store = createStore(allReducers);
-
-// if (darkMode) {
-//   document.body.classList.add("dark");
-// } else {
-//   document.body.classList.remove("dark");
-// }
-
-// const darkTheme = createTheme({
-//   palette: {
-//     type: "dark",
-//   },
-// });
-
-const lightTheme = createTheme({
-  palette: {
-    type: "light",
-  },
-});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <ThemeProvider theme={lightTheme}>
-        <Navigation />
-      </ThemeProvider>
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById("root")
 );
