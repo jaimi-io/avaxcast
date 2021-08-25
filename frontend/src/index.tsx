fe2c1561700809f29d911f88315d80a22ddc7021
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-25 12:45:34 +0100

    refactor(src/): Move `store` to index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,17 @@ import App from "App";
 // eslint-disable-next-line no-use-before-define
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import allReducers from "reducers/";
+import { createStore } from "redux";
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
