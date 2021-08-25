859a6693a97639a1817a5b7e577d35f3788d92a9
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-25 08:14:02 +0100

    fix(src/): Render `Route` without refreshing

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Markets from "components/Markets";
 import Portfolio from "components/Portfolio";
+import Navbar from "./Navbar";
 
 export default function Navigation(): JSX.Element {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Markets} />
         <Route exact path="/portfolio" component={Portfolio}></Route>
