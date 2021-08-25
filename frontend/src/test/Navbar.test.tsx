d98705905bcc0cc846bf58d4f436783a2472fa4b
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-25 23:48:54 +0100

    test(Navbar.test.tsx): Correctly setup test

--- frontend/src/test/Navbar.test.tsx
+++ frontend/src/test/Navbar.test.tsx
@@ -1,6 +1,15 @@
 import { render } from "@testing-library/react";
 import Navbar from "components/Navbar";
+import { Provider } from "react-redux";
+import store from "store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders learn react link", () => {
-  render(<Navbar />);
+  render(
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+      </Router>
+    </Provider>
+  );
 });
