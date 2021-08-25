10d9534e937635525e1c2ae9a677373a4f8a0373
Jaimi Patel <jaimip34@gmail.com>
2021-08-25 22:03:16 +0100

    feat(deploy.ts): Add `resolveMarket()` & `withdrawWinnings()` checks

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,6 @@
 import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
+import { sleep } from "./helpers";
 
 enum Market {
   AvaxUsd = 0,
@@ -21,7 +22,7 @@ const main = async (): Promise<any> => {
   const predictionMarket: Contract = await PredictionMarket.deploy(
     Market.AvaxUsd,
     50,
-    Math.floor(Date.now() / 1000) + 1200
+    Math.floor(Date.now() / 1000) + 10
   );
 
   await predictionMarket.deployed();
@@ -53,6 +54,30 @@ const main = async (): Promise<any> => {
   console.log(`Winner ${winner}`);
   console.log(
     `End time ${new Date((await predictionMarket.endTime.call()) * 1000)}`
+  );
+  console.log(
+    `Winning share price ${await predictionMarket.winningPerShare.call()}`
+  );
+  await sleep(10000);
+  const resolving = await predictionMarket.resolveMarket();
+  console.log(`resolving func ${resolving}`);
+  console.log(`isResolved ${await predictionMarket.isResolved.call()}`);
+  console.log(
+    `Winning share price ${await predictionMarket.winningPerShare.call()}`
+  );
+  console.log(`Winner ${await predictionMarket.winner.call()}`);
+  console.log(`Loser ${await predictionMarket.loser.call()}`);
+  console.log(`Num votes y ${await predictionMarket.numberShares(0)}`);
+  console.log(`Num votes n ${await predictionMarket.numberShares(1)}`);
+  console.log(
+    `Winning share price ${await predictionMarket.winningPerShare.call()}`
+  );
+  await predictionMarket.withdrawWinnings();
+  console.log(
+    `Num votes y owner ${await predictionMarket.sharesPerPerson(
+      owner.address,
+      0
+    )}`
   );
 };
 
