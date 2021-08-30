783425ca769e4c0b52db18a676d2b8ce9f9e6c3c
Jaimi Patel <jaimip34@gmail.com>
2021-08-30 13:51:16 +0100

    refactor(market.ts): Move `market.ts` to `frontend/src`
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- frontend/src/common/markets.ts
+++ frontend/src/common/markets.ts
@@ -1,4 +1,4 @@
-enum Market {
+export enum Market {
   // eslint-disable-next-line no-magic-numbers
   AVAX = 0,
   BTC,
@@ -15,4 +15,17 @@ export enum Vote {
   // eslint-disable-next-line no-magic-numbers
   Yes = 0,
   No,
+}
+
+export function menuMarkets(): string[] {
+  const ms: string[] = [];
+
+  for (const item in Market) {
+    const num = Number(item);
+    if (!isNaN(num)) {
+      ms.push(marketToString(num));
+    }
+  }
+
+  return ms;
 }
