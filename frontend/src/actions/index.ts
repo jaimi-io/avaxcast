1c5f4483a62f3f9e988200fba4fcb78cbc8b3b14
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-31 22:43:06 +0100

    feat(actions/): Add coin actions

--- frontend/src/actions/index.ts
+++ frontend/src/actions/index.ts
@@ -9,3 +9,27 @@ export function lightOn(): Action {
     type: "LIGHT ON",
   };
 }
+
+export function setAvax(): Action {
+  return {
+    type: "AVAX",
+  };
+}
+
+export function setBtc(): Action {
+  return {
+    type: "BTC",
+  };
+}
+
+export function setEth(): Action {
+  return {
+    type: "ETH",
+  };
+}
+
+export function setLink(): Action {
+  return {
+    type: "LINK",
+  };
+}
