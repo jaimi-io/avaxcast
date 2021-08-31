587ab77a53f42b5f1b56f0cefa1ae24dbf443e64
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-31 22:42:38 +0100

    feat(reducers/): Add `marketFilterReducer`

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import darkReducer from "./isDark";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
+import marketFilterReducer from "./marketFilter";
 
 const allReducers = combineReducers({
   isDark: darkReducer,
+  marketFilter: marketFilterReducer,
 });
 
 const persistConfig = {
