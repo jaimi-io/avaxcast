df4165cd984f5bcdd88c6b9491bc55ead7b54263
Jaimi Patel <jaimip34@gmail.com>
2021-09-06 12:28:54 +0100

    feat(components/): Connect contracts to market page & add route
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,8 +1,22 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Market from "./Market";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useParams,
+} from "react-router-dom";
 import AddMarket from "./AddMarket";
 import Markets from "./MarketPlace";
 import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
+
+interface PropsT {
+  address: string;
+}
+
+const MarketFunc = () => {
+  return <Market address={useParams<PropsT>().address} />;
+};
 
 export default function Navigation(): JSX.Element {
   return (
@@ -12,6 +26,7 @@ export default function Navigation(): JSX.Element {
         <Route exact path="/" component={Markets} />
         <Route exact path="/portfolio" component={Portfolio}></Route>
         <Route exact path="/addmarket" component={AddMarket}></Route>
+        <Route exact path="/market/:address" component={MarketFunc}></Route>
       </Switch>
     </Router>
   );
