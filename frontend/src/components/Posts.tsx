3bbdcb3488b5372aa449b2888ac1dfb63824f4fc
Jaimi Patel <jaimip34@gmail.com>
2021-09-06 13:10:59 +0100

    feat(src/): Remove dummy posts for real data
    Co-authored-by: R, Alex <alex.richardson19@imperial.ac.uk>

--- frontend/src/components/Posts.tsx
+++ frontend/src/components/Posts.tsx
@@ -4,80 +4,88 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Market from "common/enums";
 import { marketIcons, marketNames } from "common/markets";
-import { posts, PostsT } from "dummy";
 import { useAppSelector } from "hooks";
+import { ContractI } from "common/contract";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Post({
+  address,
   market,
   predictedPrice,
   date,
   volume,
   yesPrice,
   noPrice,
-}: PostsT): JSX.Element {
+}: ContractI): JSX.Element {
   return (
-    <Grid item>
-      <Card>
-        <CardActionArea>
-          <CardContent>
-            <Grid container spacing={3} justifyContent="center">
-              <Grid item xs>
-                <img src={marketIcons[market]} width={"50px"} />
+    <LinkContainer to={`/market/${address}`}>
+      <Grid item>
+        <Card>
+          <CardActionArea>
+            <CardContent>
+              <Grid container spacing={3} justifyContent="center">
+                <Grid item xs>
+                  <img src={marketIcons[market]} width={"50px"} />
+                </Grid>
+                <Grid item xs>
+                  <Typography component="p">{predictedPrice}</Typography>
+                </Grid>
+                <Grid item xs>
+                  <Typography component="p">{date}</Typography>
+                </Grid>
               </Grid>
-              <Grid item xs>
-                <Typography component="p">{predictedPrice}</Typography>
-              </Grid>
-              <Grid item xs>
-                <Typography component="p">{date}</Typography>
-              </Grid>
-            </Grid>
 
-            <Typography variant="h6" component="p">
-              {`Will ${marketNames[market]} reach ${predictedPrice} by ${date}`}
-            </Typography>
-            <Grid container>
-              <Grid item xs>
-                <Typography variant="body2" component="p">
-                  {"Total Volume:"}
-                </Typography>
-                <Typography component="p">{volume}</Typography>
-              </Grid>
-              <Grid item xs>
-                <Grid container>
-                  <Grid item xs={12}>
-                    <Typography
-                      variant="body1"
-                      component="p"
-                      align="right">{`Yes: ${yesPrice}`}</Typography>
-                  </Grid>
+              <Typography variant="h6" component="p">
+                {`Will ${marketNames[market]} reach ${predictedPrice} by ${date}`}
+              </Typography>
+              <Grid container>
+                <Grid item xs>
+                  <Typography variant="body2" component="p">
+                    {"Total Volume:"}
+                  </Typography>
+                  <Typography component="p">{volume}</Typography>
+                </Grid>
+                <Grid item xs>
+                  <Grid container>
+                    <Grid item xs={12}>
+                      <Typography
+                        variant="body1"
+                        component="p"
+                        align="right">{`Yes: ${yesPrice}`}</Typography>
+                    </Grid>
 
-                  <Grid item xs={12}>
-                    <Typography
-                      variant="body1"
-                      component="p"
-                      align="right">{`No: ${noPrice}`}</Typography>
+                    <Grid item xs={12}>
+                      <Typography
+                        variant="body1"
+                        component="p"
+                        align="right">{`No: ${noPrice}`}</Typography>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Grid>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    </LinkContainer>
   );
 }
 
-function Posts(): JSX.Element {
+interface PropsT {
+  contracts: ContractI[];
+}
+
+function Posts({ contracts }: PropsT): JSX.Element {
   const marketFilter = useAppSelector((state) => state.marketFilter);
 
-  const filterPost = (post: PostsT) => {
+  const filterPost = (post: ContractI) => {
     if (marketFilter === Market.ALL) {
       return true;
     }
     return post.market === marketFilter;
   };
 
-  const filtered = posts.filter(filterPost);
+  const filtered = contracts.filter(filterPost);
 
   return (
     <div style={{ marginTop: 20, padding: 50 }}>
