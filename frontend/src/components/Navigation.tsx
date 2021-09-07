86318df8c51716759a5ac8c79a5214783d63f3fd
R, Alex <alex.richardson19@imperial.ac.uk>
2021-09-07 23:03:00 +0100

    feat(components/): Add 404 page

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import Market from "./Market";
 import {
   BrowserRouter as Router,
+  Redirect,
   Route,
   Switch,
   useParams,
@@ -9,6 +10,7 @@ import AddMarket from "./AddMarket";
 import MarketPlace from "./MarketPlace";
 import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
+import NotFound from "./NotFound";
 
 interface PropsT {
   address: string;
@@ -24,9 +26,11 @@ export default function Navigation(): JSX.Element {
       <Navbar />
       <Switch>
         <Route exact path="/" component={MarketPlace} />
-        <Route exact path="/portfolio" component={Portfolio}></Route>
-        <Route exact path="/addmarket" component={AddMarket}></Route>
-        <Route exact path="/market/:address" component={MarketFunc}></Route>
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/addmarket" component={AddMarket} />
+        <Route exact path="/market/:address" component={MarketFunc} />
+        <Route exact path="/404" component={NotFound} />
+        <Redirect to="/404" />
       </Switch>
     </Router>
   );
