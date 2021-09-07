fc8389e6cfe0fea24ffed6eb4b76066975bd6635
R, Alex <alex.richardson19@imperial.ac.uk>
2021-09-08 00:21:26 +0100

    refactor(components/): Extract code to `<Loading/>`

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,28 @@
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: "#fff",
+    },
+  })
+);
+
+interface PropsT {
+  isLoading: boolean;
+}
+
+function Loading({ isLoading }: PropsT): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <Backdrop className={classes.backdrop} open={isLoading}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+}
+
+export default Loading;
