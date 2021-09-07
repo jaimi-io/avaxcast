244d4a49c55949a34f1ea8200aad1d37fad6712d
R, Alex <alex.richardson19@imperial.ac.uk>
2021-09-07 12:56:19 +0100

    refactor(components/): Extract code to `SuccessSnackbar`

---
+++ frontend/src/components/SuccessSnackbar.tsx
@@ -0,0 +1,36 @@
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
+import Snackbar from "@material-ui/core/Snackbar";
+import { SyntheticEvent } from "react";
+
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+interface PropsT {
+  successMsg: string;
+  failMsg: string;
+  success: boolean;
+  open: boolean;
+  handleClose: (
+    event?: SyntheticEvent<Element, Event> | undefined,
+    reason?: string | undefined
+  ) => void;
+}
+
+function SuccessSnackbar({
+  successMsg,
+  failMsg,
+  success,
+  open: openSnackbar,
+  handleClose,
+}: PropsT): JSX.Element {
+  return (
+    <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity={success ? "success" : "error"}>
+        {success ? `${successMsg}` : `${failMsg}`}
+      </Alert>
+    </Snackbar>
+  );
+}
+
+export default SuccessSnackbar;
