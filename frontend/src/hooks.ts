29642f3217272b98a4b561470c205b82c4b32450
R, Alex <alex.richardson19@imperial.ac.uk>
2021-09-09 17:16:07 +0100

    feat(hooks.ts): Add `useFecth`
    Co-authored-by: Jaimi Patel <jaimip34@gmail.com>

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -1,5 +1,9 @@
+import { ContractI, getContractInfo } from "common/contract";
+import { Market, Vote } from "common/enums";
+import { useEffect, useState } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "store";
+import { toBN } from "web3-utils";
 
 // Use throughout app instead of plain `useDispatch` and `useSelector`
 
@@ -11,3 +15,52 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
  * Custom useDispatch hook with type safety
  */
 export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
+
+/**
+ * Cache used to store previous information for contracts
+ */
+interface CacheI {
+  [key: string]: ContractI;
+}
+
+const cache: CacheI = {};
+const dummy: ContractI = {
+  market: Market.ALL,
+  predictedPrice: "$X",
+  date: new Date(),
+  volume: toBN(0),
+  yesPrice: toBN(0),
+  noPrice: toBN(0),
+  address: "0xfe6...",
+  isResolved: false,
+  winner: Vote.Yes,
+  winningPerShare: toBN(0),
+};
+
+/**
+ * Used to fetch information on the contract and store it
+ * @param address - The address of the contract
+ * @returns Information on the {@link ContractI}
+ */
+export const useFetch = (address: string): ContractI => {
+  const [data, setData] = useState<ContractI>(dummy);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+
+    const fetchData = async () => {
+      if (cache[address]) {
+        setData(cache[address]);
+      }
+      const data = await getContractInfo(address);
+      cache[address] = data;
+      setData(cache[address]);
+    };
+
+    fetchData();
+  }, [address]);
+
+  return data;
+};
