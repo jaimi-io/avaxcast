8ecbf6d20ba2b94389a84d9f442200eab41bca95
R, Alex <alex.richardson19@imperial.ac.uk>
2021-09-09 17:58:18 +0100

    feat(reducers/): Add `isLoading` reducer

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -3,12 +3,14 @@ import darkReducer from "./isDark";
 import storage from "redux-persist/lib/storage";
 import { persistReducer } from "redux-persist";
 import marketFilterReducer from "./marketFilter";
+import loadingReducer from "./isLoading";
 
 /**
  * Combination of all the reducers
  */
 const allReducers = combineReducers({
   isDark: darkReducer,
+  isLoading: loadingReducer,
   marketFilter: marketFilterReducer,
 });
 
